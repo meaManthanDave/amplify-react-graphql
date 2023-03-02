7c0c9b2975366718c74f554c6174ae63185634d4
manthan dave <manthan.3waysource@gmail.com>
2023-03-02 14:48:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from manthan
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
-// import logo from "./logo.svg";
-// import "@aws-amplify/ui-react/styles.css";
-// import {
-//   withAuthenticator,
-//   Button,
-//   Heading,
-//   Image,
-//   View,
-//   Card,
-// } from "@aws-amplify/ui-react";
-
-// function App({ signOut }) {
+// function App() {
 //   return (
-//     <View className="App">
-//       <Card>
-//         <Image src={logo} className="App-logo" alt="logo" />
-//         <Heading level={1}>We now have Auth!</Heading>
-//       </Card>
-//       <Button onClick={signOut}>Sign Out</Button>
-//     </View>
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello from manthan
+//         </p>
+//       </header>
+//     </div>
 //   );
 // }
 
-// export default withAuthenticator(App);
+// export default App;
+
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+export default withAuthenticator(App);
